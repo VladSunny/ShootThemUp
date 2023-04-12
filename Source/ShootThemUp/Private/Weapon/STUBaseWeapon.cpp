35cd0a39b7bdacbf8c82883a3ac93ee9ae359229
Vlad Sunny <68503186+VladSunny@users.noreply.github.com>
2023-04-12 08:54:04 +0300

    47. Tracing a line

--- Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
@@ -2,6 +2,10 @@
 
 #include "Weapon/STUBaseWeapon.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/Controller.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogBaseCharacter, All, All);
 
@@ -18,10 +22,46 @@ ASTUBaseWeapon::ASTUBaseWeapon()
 void ASTUBaseWeapon::BeginPlay()
 {
 	Super::BeginPlay();
-	
+    check(WeaponMesh);
 }
 
 void ASTUBaseWeapon::Fire(){
     UE_LOG(LogBaseCharacter, Display, TEXT("Fire!"));
+
+	MakeShot();
 }
 
+void ASTUBaseWeapon::MakeShot(){
+    if (!GetWorld()) return;
+
+    const auto Player = Cast<ACharacter>(GetOwner());
+    if (!Player) return;
+
+    const auto Controller = Player->GetController<APlayerController>();
+    if (!Controller) return;
+
+    FVector ViewLocation;
+    FRotator ViewRotator;
+    Controller->GetPlayerViewPoint(ViewLocation, ViewRotator);
+
+	const FTransform SocketTransform = WeaponMesh->GetSocketTransform(MuzzleSocketName);
+    const FVector TraceStart = ViewLocation; //SocketTransform.GetLocation();
+    const FVector ShootDirection = ViewRotator.Vector(); // SocketTransform.GetRotation().GetForwardVector();
+    const FVector TraceEnd = TraceStart + ShootDirection * TraceMaxDistance;
+
+    FCollisionQueryParams CollisionParams;
+    CollisionParams.AddIgnoredActor(GetOwner());
+
+    FHitResult HitResult;
+    GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ECollisionChannel::ECC_Visibility, CollisionParams);
+
+    if (HitResult.bBlockingHit){
+        DrawDebugLine(GetWorld(), SocketTransform.GetLocation(), HitResult.ImpactPoint, FColor::Red, false, 3.0f, 0, 3.0f);
+        DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10.0f, 24, FColor::Red, false, 5.0f);
+
+        UE_LOG(LogBaseCharacter, Display, TEXT("bone: %s"), *HitResult.BoneName.ToString());
+    }
+    else{
+        DrawDebugLine(GetWorld(), SocketTransform.GetLocation(), TraceEnd, FColor::Red, false, 3.0f, 0, 3.0f);
+    }
+}
--- Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -22,6 +22,13 @@ protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
     USkeletalMeshComponent* WeaponMesh;
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+    FName MuzzleSocketName = "MuzzleSocket";
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+    float TraceMaxDistance = 30000.0f;
+
 	virtual void BeginPlay() override;
 
+	void MakeShot();
 };
