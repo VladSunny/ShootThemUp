74161f07ebba852cb570f2d0c98c030ac63fff16
Vlad Sunny <68503186+VladSunny@users.noreply.github.com>
2023-04-27 08:12:24 +0300

    71. User Widget for observer mode. Animations in UMG
    Text animation for deadText doesnt work

--- Source/ShootThemUp/Private/UI/STUPlayerHUDWidget.cpp
+++ Source/ShootThemUp/Private/UI/STUPlayerHUDWidget.cpp
@@ -6,11 +6,7 @@
 
 float USTUPlayerHUDWidget::GetHealthPrecent() const
 {
-    const auto Player = GetOwningPlayerPawn();
-    if (!Player) return 0.0f;
-
-    const auto Component = Player->GetComponentByClass(USTUHealthComponent::StaticClass());
-    const auto HealthComponent = Cast<USTUHealthComponent>(Component);
+    const auto HealthComponent = GetHealthComponent();
     if (!HealthComponent) return 0.0f;
 
     return HealthComponent->GetHealthPrecent();
@@ -32,6 +28,18 @@ bool USTUPlayerHUDWidget::GetCurrentWeaponAmmoData(FAmmoData& AmmoData) const
     return WeaponComponent->GetCurrentWeaponAmmoData(AmmoData);
 }
 
+bool USTUPlayerHUDWidget::IsPlayerAlive() const
+{
+    const auto HealthComponent = GetHealthComponent();
+    return HealthComponent && !HealthComponent->IsDead();
+}
+
+bool USTUPlayerHUDWidget::IsPlayerSpectating() const
+{
+    const auto Controller = GetOwningPlayer();
+    return Controller && Controller->GetStateName() == NAME_Spectating;
+}
+
 USTUWeaponComponent* USTUPlayerHUDWidget::GetWeaponComponent() const
 {
     const auto Player = GetOwningPlayerPawn();
@@ -40,4 +48,13 @@ USTUWeaponComponent* USTUPlayerHUDWidget::GetWeaponComponent() const
     const auto Component = Player->GetComponentByClass(USTUWeaponComponent::StaticClass());
     const auto WeaponComponent = Cast<USTUWeaponComponent>(Component);
     return WeaponComponent;
+}
+USTUHealthComponent* USTUPlayerHUDWidget::GetHealthComponent() const
+{
+    const auto Player = GetOwningPlayerPawn();
+    if (!Player) return nullptr;
+
+    const auto Component = Player->GetComponentByClass(USTUHealthComponent::StaticClass());
+    const auto HealthComponent = Cast<USTUHealthComponent>(Component);
+    return HealthComponent;
 }
