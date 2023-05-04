ce73c624c9f899092201f93d85aa9caaede84c05
Vlad Sunny <68503186+VladSunny@users.noreply.github.com>
2023-05-04 07:53:27 +0300

    79. Building the game

--- Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -21,11 +21,15 @@ void ASTUBasePickup::BeginPlay()
     Super::BeginPlay();
 
     check(CollisionComponent);
+
+    GenerateRotationYaw();
 }
 
 void ASTUBasePickup::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+
+    AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f));
 }
 
 void ASTUBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
@@ -57,9 +61,15 @@ void ASTUBasePickup::PickupWasTaken()
 
 void ASTUBasePickup::Respawn()
 {
+    GenerateRotationYaw();
     CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
     if (GetRootComponent())
     {
         GetRootComponent()->SetVisibility(true, true);
     }
+}
+
+void ASTUBasePickup::GenerateRotationYaw() {
+    const auto Direction = FMath::RandBool() ? 1.0f : -1.0f;
+    RotationYaw = FMath::RandRange(1.0f, 2.0f) * Direction;
 }
