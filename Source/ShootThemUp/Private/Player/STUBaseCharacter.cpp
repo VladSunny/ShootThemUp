723ab45d3975366deaebd6e2066a809c76c6e6bb
Vlad Sunny <68503186+VladSunny@users.noreply.github.com>
2023-05-07 13:15:48 +0300

    87. UI effect of blood

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -49,7 +49,7 @@ void ASTUBaseCharacter::BeginPlay()
     check(WeaponComponent);
     check(GetMesh());
 
-    OnHealthChanged(HealthComponent->GetHealth());
+    OnHealthChanged(HealthComponent->GetHealth(), 0.0f);
 
     HealthComponent->OnDeath.AddUObject(this, &ASTUBaseCharacter::OnDeath);
     HealthComponent->OnHealthChanged.AddUObject(this, &ASTUBaseCharacter::OnHealthChanged);
@@ -57,7 +57,7 @@ void ASTUBaseCharacter::BeginPlay()
     LandedDelegate.AddDynamic(this, &ASTUBaseCharacter::OnGroundLanded);
 }
 
-void ASTUBaseCharacter::OnHealthChanged(float Health)
+void ASTUBaseCharacter::OnHealthChanged(float Health, float HealthDelta)
 {
     HealthTextComponent->SetText(FText::FromString(FString::Printf(TEXT("%.0f"), Health)));
 }
