bc17a741ea5c1896f8b7e86327daffb33d4aa570
Vlad Sunny <68503186+VladSunny@users.noreply.github.com>
2023-05-08 22:43:40 +0300

    90. Niagara. The effect of bullet marks

--- Source/ShootThemUp/Public/Weapon/STURifleWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STURifleWeapon.h
@@ -8,6 +8,7 @@
 
 class USTUWeaponFXComponent;
 class UNiagaraComponent;
+class UNiagaraSystem;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTURifleWeapon : public ASTUBaseWeapon
@@ -22,14 +23,20 @@ public:
 
 protected:
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Weapon")
     float TimeBetweenShots = 0.1f;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
     float BulletSpread = 1.0f;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
     float DamageAmount = 10.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "VFX")
+    UNiagaraSystem* TraceFX;
+
+    UPROPERTY(EditDefaultsOnly, Category = "VFX")
+    FString TraceTargetName = "TraceTarget";
 
     UPROPERTY(VisibleAnywhere, Category = "VFX")
     USTUWeaponFXComponent* WeaponFXComponent;
@@ -47,4 +54,5 @@ private:
     void MakeDamage(const FHitResult& HitResult);
     void InitMuzzleFX();
     void SetMuzzleFXVisibility(bool Visible);
+    void SpawnTraceFX(const FVector& TraceStart, const FVector& TraceEnd);
 };
