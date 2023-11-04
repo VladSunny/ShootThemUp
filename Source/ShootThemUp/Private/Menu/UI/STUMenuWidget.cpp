9f3f33c5578ac89c13a526fd6717a4cdc6938875
Vladislav Sungurov <vladislavsunny@gmail.com>
2023-11-04 21:09:59 +0300

    130. Уровень главного меню

---
+++ Source/ShootThemUp/Private/Menu/UI/STUMenuWidget.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Menu/UI/STUMenuWidget.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+
+void USTUMenuWidget::NativeOnInitialized()
+{
+    Super::NativeOnInitialized();
+
+    if (StartGameButton)
+    {
+        StartGameButton->OnClicked.AddDynamic(this, &USTUMenuWidget::OnStartGame);
+    }
+}
+
+void USTUMenuWidget::OnStartGame()
+{
+    const FName StartupLevelName = "Test";
+    UGameplayStatics::OpenLevel(this, StartupLevelName);
+}
